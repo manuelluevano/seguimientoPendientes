ac579b649de685d09f3a1a272996342203755782
Manuel  Luevano <manuelluevano@Manuels-MacBook-Pro.local>
2023-07-04 00:37:18 -0600

    Header acomodado para resoluciones pequenas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <h1 className="font-black text-5xl mt-10 text-center w-1/2 mx-auto md:w-1/3">
+      <h1 className="font-black text-5xl mt-10 text-center  mx-auto md:w-1/3">
         Seguimiento de <span className="text-orange-600">Servicios</span>
       </h1>
     </>
